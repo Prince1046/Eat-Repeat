d5086109e85e67e14837a5045a626e468d1a0b8e
Prince1046 <97391161+Prince1046@users.noreply.github.com>
2023-07-04 16:41:51 +0530

    Update db.js

--- backend/db.js
+++ backend/db.js
@@ -1,6 +1,6 @@
 const mongoose= require('mongoose');
 
-const mongoURI='mongodb://greymatter:Acer05feb@ac-fkspo3r-shard-00-00.db5b2tf.mongodb.net:27017,ac-fkspo3r-shard-00-01.db5b2tf.mongodb.net:27017,ac-fkspo3r-shard-00-02.db5b2tf.mongodb.net:27017/foodapp?ssl=true&replicaSet=atlas-q93itd-shard-0&authSource=admin&retryWrites=true&w=majority'
+const mongoURI='mongodb://greymatter:@ac-fkspo3r-shard-00-00.db5b2tf.mongodb.net:27017,ac-fkspo3r-shard-00-01.db5b2tf.mongodb.net:27017,ac-fkspo3r-shard-00-02.db5b2tf.mongodb.net:27017/foodapp?ssl=true&replicaSet=atlas-q93itd-shard-0&authSource=admin&retryWrites=true&w=majority'
 // console.log("hell");
 const mongoDB = async () => {
   await mongoose.connect(mongoURI,{useNewUrlParser: true}, async(err,result)=>{
